ebefdaedefa0c5fc2aaaccb0f5dd998330196167
GettyWinslo <gettygreenllc2020@gmail.com>
2022-03-01 17:40:18 -0600

    initial commit

---
+++ SolarStart/js/main.js
@@ -0,0 +1,91 @@
+/*jslint browser:true */
+"use strict";
+function addMonths(elem) {
+  var annualUseKw = 0,
+    dailyUseKw = 0,
+    i = 0,
+    x = 0,
+    y = 0;
+  var months = document.getElementById(elem).getElementsByTagName("input");
+
+  for (i = 0; i < months.length; i++) {
+    x = Number(months[i].value);
+    annualUseKw += x;
+  } //end loop
+  dailyUseKw = annualUseKw / 365;
+  return dailyUseKw;
+} //end of function
+
+function sunHrs() {
+  var hrs;
+  var theZone = document.forms.solarForm.zone.selectedIndex;
+  theZone += 1;
+
+  switch (theZone) {
+    case 1:
+      hrs = 6;
+      break;
+    case 2:
+      hrs = 5.5;
+      break;
+    case 3:
+      hrs = 5;
+      break;
+    case 4:
+      hrs = 4.5;
+      break;
+    case 5:
+      hrs = 4.2;
+      break;
+    case 6:
+      hrs = 3.5;
+      break;
+    default:
+      hrs = 0;
+  } //End of switch
+  return hrs;
+} //end of function
+
+function calculatePanel() {
+  var userChoice = document.forms.solarForm.panel.selectedIndex;
+  var panelOptions = document.forms.solarForm.panel.options;
+  var power = panelOptions[userChoice].value;
+  var name = panelOptions[userChoice].text;
+  var x = [power, name];
+  return x;
+} //end of function
+
+function calculateSolar() {
+  var dailyUseKw = addMonths("mpc");
+  
+
+  var sunHrsPerDay = sunHrs();
+  
+
+  var minKwNeeds = dailyUseKw / sunHrsPerDay;
+  
+
+  var realKwNeeds = minKwNeeds * 1.25;
+  
+  var realWattNeeds = realKwNeeds * 1000;
+  
+
+  var panelInfo = calculatePanel();
+  var panelOutput = panelInfo[0];
+  var panelName = panelInfo[1];
+  
+
+  var panelsNeeded = Math.ceil(realWattNeeds / panelOutput);
+  
+
+  var feedback = "";
+  feedback +=
+    "<p>Based on your average daily use of "+Math.round(dailyUseKw)+" kWh,you will need to purchase "+panelsNeeded+" "+panelName+" solar panels to offset 100% of you electricity bill.</p>";
+  feedback += "<h2>Additional Details</h2>";
+  feedback +="<p>Your average daily electricity consumption:"+Math.round(dailyUseKw) +" Kwh per day.</p>";
+  feedback += "<p>Average sunshine hours per day:"+sunHrsPerDay+" hours</p>";
+  feedback += "<p>Realistic watts needed per hour:"+Math.round(realWattNeeds) +" watts/hour.</p>";
+  feedback +=
+    "<p>The "+panelName+" panel you selected generates about "+panelOutput+" watts per hour.</p>";
+  document.getElementById("feedback").innerHTML = feedback;
+} //end of function
